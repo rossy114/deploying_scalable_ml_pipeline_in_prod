b3d099e584440e76f89b393988c962671cc020c1
Ross <rf@rosss-mbp.home>
2022-09-06 00:25:41 +0100

    updating data folder structure

---
+++ nd0821-c3-starter-code/starter/clean/basic_clean.py
@@ -0,0 +1,19 @@
+"""
+Basic cleaning
+"""
+import pandas as pd
+
+def clean():
+"""
+Remove whitespaces
+"""
+	df = pd.read_csv("data//raw/census.csv")
+
+	df.columns = df.columns.str.strip()
+
+	df.to_csv("data/clean/census.csv", index=False)
+
+
+
+# df = df.drop_duplicates()
+# len(df)-len(df.drop_duplicates())
